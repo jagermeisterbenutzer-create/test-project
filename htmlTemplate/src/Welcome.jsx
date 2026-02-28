95f47dbe978e2895a9b87d6f17b1899294e2e6ac
Sasha20055 <nikolalex783@gmail.com>
2026-02-28 19:11:27 +0300

    Replaced Vite+React default page with a custom Welcome page

---
+++ htmlTemplate/src/Welcome.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Welcome() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to the Custom Page!</h1>
+      <p>This page replaced the default Vite+React template.</p>
+    </div>
+  );
+}
+
+export default Welcome;
